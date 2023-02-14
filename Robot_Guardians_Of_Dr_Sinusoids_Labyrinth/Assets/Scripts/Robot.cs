200c8b4b1c056aaa4c3b3588d55cbe86a4cf9e7f
brimstonetrader <106700187+brimstonetrader@users.noreply.github.com>
2023-02-13 20:20:06 -0600

    Finished

--- Robot_Guardians_Of_Dr_Sinusoids_Labyrinth/Assets/Scripts/Robot.cs
+++ Robot_Guardians_Of_Dr_Sinusoids_Labyrinth/Assets/Scripts/Robot.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Robot : MonoBehaviour
+
 {
 
     public string text;
+    private AudioSource audio;
+
 
     public void OnTriggerEnter2D(Collider2D collider2D) {
         print("Entered..");
+        audio.Play();
         if (collider2D.gameObject.CompareTag("Player")) {
             GameManager.Instance.DialogShow(text);
         }
@@ -23,7 +28,7 @@ public class Robot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
